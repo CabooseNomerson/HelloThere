649321e1e6b7152cb40eeaab3e2389da2064311e
CabooseNomerson <CabooseNomerson@Brandons-MacBook-Pro.local>
2019-04-04 17:48:47 -0400

    4/4/19 - Comiting Android Studio work after class

--- _ANDROIDGITHUB/app/src/main/java/com/example/listmaker/Categories.java
+++ _ANDROIDGITHUB/app/src/main/java/com/example/listmaker/Categories.java
@@ -1,20 +1,16 @@
 package com.example.listmaker;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
-    RelativeLayout GuestSignInButton = (RelativeLayout) findViewById(R.id.GuestSignInButton);
-
+public class Categories extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Home"); //title of actionBar
+        setTitle("Categories"); //title of actionBar
         ActionBar ab = getSupportActionBar();
         //ab.setLogo();//for setting logo on the action bar
         //ab.setDisplayUseLogoEnabled(true);//displays logo
