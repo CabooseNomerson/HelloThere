25bfa58c1028ea9ee5a48a560dc033edf42c8e84
CabooseNomerson <CabooseNomerson@Brandons-MacBook-Pro.local>
2019-04-15 13:28:48 -0400

    4/15/19 - working on Android Studio project, redesigning the code to work better on the emulator

--- _ANDROIDGITHUB/app/src/main/java/com/example/listmaker/Category.java
+++ _ANDROIDGITHUB/app/src/main/java/com/example/listmaker/Category.java
@@ -9,5 +9,9 @@ public class Category extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        setTitle("Category"); //title of actionBar
     }
+
+
+
 }
