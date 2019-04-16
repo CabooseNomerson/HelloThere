f8fba936024bbda1eb7f471cb5f57b9103194932
CabooseNomerson <CabooseNomerson@Brandons-MacBook-Pro.local>
2019-04-16 13:58:08 -0400

    4/16/19 - Committing before presentation; created about 80% of the second screen's content; experimented with custom action bar but that didn't go anywhere; ready for presentation; organized and labeled the contents of the XML documents for better readability

--- _ANDROIDGITHUB/app/src/main/java/com/example/listmaker/Category.java
+++ _ANDROIDGITHUB/app/src/main/java/com/example/listmaker/Category.java
@@ -2,6 +2,7 @@ package com.example.listmaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Category extends AppCompatActivity {
 
@@ -9,7 +10,11 @@ public class Category extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
-        setTitle("Category"); //title of actionBar
+        setTitle("List Categories"); //title of actionBar
+
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(myToolbar);
+
     }
 
 
